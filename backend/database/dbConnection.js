750e50c4d1eb9cb9dcc3361f156b745b7d8d9377
Manish Chaudhary <chaudharymanish120@gmail.com>
2024-03-05 04:34:22 +0530

    last commit

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,13 +1,37 @@
+// import mongoose from "mongoose";
+
+// export const dbConnection = () => {
+//   mongoose.connect(process.env.MONGO_URI, {
+//       dbName: "MERN_JOB_SEEKING_WEBAPP",
+//     })
+//     .then(() => {
+//       console.log("Connected to database.");
+//     })
+//     .catch((err) => {
+//       console.log(`Some Error occured. ${err}`);
+//     });
+// };
+
+
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
+  // Check if the MONGO_URI environment variable is defined
+  if (!process.env.MONGO_URI) {
+    console.error("MONGO_URI environment variable is not defined.");
+    return; // Stop execution if MONGO_URI is not defined
+  }
+
   mongoose.connect(process.env.MONGO_URI, {
-      dbName: "MERN_JOB_SEEKING_WEBAPP",
-    })
-    .then(() => {
-      console.log("Connected to database.");
-    })
-    .catch((err) => {
-      console.log(`Some Error occured. ${err}`);
-    });
+    dbName: "MERN_JOB_SEEKING_WEBAPP",
+    useNewUrlParser: true, // Add this option to avoid deprecation warnings
+    useUnifiedTopology: true // Add this option for modern server discovery and monitoring
+  })
+  .then(() => {
+    console.log("Connected to database.");
+  })
+  .catch((err) => {
+    console.error("Error connecting to database:", err);
+  });
 };
+
