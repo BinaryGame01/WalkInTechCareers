26299fe380583015fac1c7b1cba88b323888168d
Manish Chaudhary <chaudharymanish120@gmail.com>
2024-03-06 04:24:19 +0530

    final1

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,6 @@ import app from "./app.js";
 app.listen(process.env.PORT || 4000 , () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send(`<h1> This is HOMEPAGE Baby 01 </h1>`);
+});
