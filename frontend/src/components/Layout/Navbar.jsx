31297de543a0f80a9806f453053ddc1ddb76fced
Manish Chaudhary <chaudharymanish120@gmail.com>
2024-03-06 05:05:40 +0530

    last1234

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://kind-puce-sea-lion-wrap.cyclic.app/api/v1/user/logout",
         {
           withCredentials: true,
         }
