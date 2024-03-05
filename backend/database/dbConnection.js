48979d91dca408b5333360ead449cc215cf65ef9
Manish Chaudhary <chaudharymanish120@gmail.com>
2024-03-05 15:15:35 +0530

    last5

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,21 +1,22 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
-  // Check if the MONGO_URI environment variable is defined
-  if (!process.env.MONGO_URI) {
-    console.error("MONGO_URI environment variable is not defined.");
-    return; // Stop execution if MONGO_URI is not defined
-  }
+export const dbConnection = async () => {
+  try {
+    // Check if the MONGO_URI environment variable is defined
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI environment variable is not defined.");
+    }
 
-  mongoose.connect(process.env.MONGO_URI, {
-    dbName: "MERN_JOB_SEEKING_WEBAPP"
-  })
-  .then(() => {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "MERN_JOB_SEEKING_WEBAPP",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log("Connected to database.");
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error("Error connecting to database:", err);
-  });
+    // Exit the process if there's an error connecting to the database
+    process.exit(1);
+  }
 };
-
-
