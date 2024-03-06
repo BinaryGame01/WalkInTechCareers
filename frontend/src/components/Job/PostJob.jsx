e6c63e5781f706796fda38baee8b04beea408005
Manish Chaudhary <chaudharymanish120@gmail.com>
2024-03-06 15:13:59 +0530

    last and final

--- frontend/src/components/Job/PostJob.jsx
+++ frontend/src/components/Job/PostJob.jsx
@@ -31,7 +31,7 @@ const PostJob = () => {
     }
     await axios
       .post(
-        "https://kind-puce-sea-lion-wrap.cyclic.app/api/v1/job/post",
+        "http://localhost:4000/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
